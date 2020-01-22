e06250d513d5b3fa656c232a78c3aa1a94453c34
Nestor <nestor.bolanos102@alu.ulpgc.es>
2020-01-22 12:38:39 +0000

    Parameterized Tests

--- src/test/java/Strongest_.java
+++ src/test/java/Strongest_.java
@@ -1,11 +1,21 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(org.junit.runners.Parameterized.class)
 public class Strongest_ {
 
     private Strongest strongest;
+    private final int[] array;
+    private final int number;
+
+    public Strongest_(int[] array, int number) {
+        this.array = array;
+        this.number = number;
+    }
 
     @Before
     public void setUp() {
@@ -13,40 +23,23 @@ public class Strongest_ {
     }
 
     @Test
-    public void given_empty_array_should_return_0() {
-        assertThat(strongest.of(new int[]{})).isEqualTo(0);
+    public void execute() {
+        assertThat(strongest.of(array)).isEqualTo(number);
     }
 
-    @Test
-    public void given_less_or_more_than_two_elements_should_return_0() {
-        assertThat(strongest.of(new int[]{1, 2, 3})).isEqualTo(0);
-        assertThat(strongest.of(new int[]{1})).isEqualTo(0);
-    }
-
-    @Test
-    public void given_second_element_less_or_equal_than_previous_should_return_0() {
-        assertThat(strongest.of(new int[]{2, 2})).isEqualTo(0);
-        assertThat(strongest.of(new int[]{2, 1})).isEqualTo(0);
-    }
-
-    @Test
-    public void given_first_number_negative_should_return_0() {
-        assertThat(strongest.of(new int[]{-1, 0})).isEqualTo(0);
-    }
-
-    @Test
-    public void given_1_2_should_return_0() {
-        assertThat(strongest.of(new int[]{1, 2})).isEqualTo(2);
-    }
-
-    @Test
-    public void given_5_10_should_return_0() {
-        assertThat(strongest.of(new int[]{5, 10})).isEqualTo(8);
-    }
-
-    @Test
-    public void given_48_56_should_return_0() {
-        assertThat(strongest.of(new int[]{48, 56})).isEqualTo(48);
+    @Parameterized.Parameters
+    public static Object[][] cases() {
+        return new Object[][]{
+                {new int[]{}, 0},
+                {new int[]{1, 2, 3}, 0},
+                {new int[]{1}, 0},
+                {new int[]{2, 2}, 0},
+                {new int[]{2, 1}, 0},
+                {new int[]{-1, 0}, 0},
+                {new int[]{1, 2}, 2},
+                {new int[]{5, 10}, 8},
+                {new int[]{48, 56}, 48},
+        };
     }
 
 }
