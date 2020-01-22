4b76d76559c4dfa015735e6d8bcc265b284ee98d
Nestor <nestor.bolanos102@alu.ulpgc.es>
2020-01-22 10:05:38 +0000

    Second Test: given_less_or_more_than_two_elements

--- src/test/java/Strongest_.java
+++ src/test/java/Strongest_.java
@@ -17,4 +17,12 @@ public class Strongest_ {
         assertThat(strongest.of(new int[]{})).isEqualTo(0);
     }
 
+    @Test
+    public void given_less_or_more_than_two_elements_should_return_0() {
+        assertThat(strongest.of(new int[]{1, 2, 3})).isEqualTo(0);
+        assertThat(strongest.of(new int[]{1})).isEqualTo(0);
+    }
+
+
+
 }
